4f519c1ca2943929b57019b14c1df2e8f551f2f8
coachjackferg <133680445+coachjackferg@users.noreply.github.com>
2025-01-23 03:01:34 -0500

    Clean up provider completion handling, rework middleware

---
+++ src/types/middleware.ts
@@ -0,0 +1,36 @@
+import {
+    MagmaAssistantMessage,
+    MagmaState,
+    MagmaToolCall,
+    MagmaToolResult,
+    MagmaToolResultMessage,
+    MagmaUserMessage,
+} from './index';
+
+export const MagmaMiddlewareTriggers = [
+    'onCompletion',
+    'preCompletion',
+    'onToolExecution',
+    'preToolExecution',
+] as const;
+
+export type MagmaMiddlewareTriggerType = (typeof MagmaMiddlewareTriggers)[number];
+
+export type MagmaMiddleware = {
+    trigger: MagmaMiddlewareTriggerType;
+    action: (
+        message: MagmaUserMessage | MagmaAssistantMessage | MagmaToolCall | MagmaToolResult,
+        state?: MagmaState
+    ) => Promise<string | void> | string | void;
+};
+
+export type MagmaMiddlewareReturnType<T extends MagmaMiddlewareTriggerType> =
+    T extends 'preCompletion'
+        ? string
+        : T extends 'onCompletion'
+          ? string
+          : T extends 'preToolExecution'
+            ? MagmaToolResultMessage
+            : T extends 'onToolExecution'
+              ? MagmaToolResultMessage
+              : never;

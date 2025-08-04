01bd3ffc6f2f7e047983a6b6986d48127f491b39
Matty Hogan <matthewhogan63@gmail.com>
2025-08-04 17:13:47 -0400

    Add setup callback to @hook

--- src/types/utilities/hooks.ts
+++ src/types/utilities/hooks.ts
@@ -5,5 +5,6 @@ export type MagmaHook = {
     name: string;
     handler: (request: Request, response: Response, agent: MagmaAgent) => Promise<void>;
     session?: 'default' | (string & {}) | ((req: Request) => string | Promise<string>);
+    setup?: ((req: Request) => Record<string, any> | Promise<Record<string, any>>) | Record<string, any>;
     description?: string;
 };
